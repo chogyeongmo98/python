c99e756869c0e104188467f93dc334eca9ae7a17
chogyeongmo98 <62549652+chogyeongmo98@users.noreply.github.com>
2020-04-05 23:57:44 +0900

    Create python1.py

---
+++ python1.py
@@ -0,0 +1,17 @@
+#n_value = input("함수를 입력하세요") #strint-->n_value
+#n_value = int(n_value)#strint-->int 타입 형변환 
+#print("출력 = ", n_value)
+
+#if n_value > 10 :
+# print("입력값이 10보다 크다")
+#else : 
+#    print("입력값이 10보다 작다")
+
+i = 0
+while i<10 :
+    print("i is =",i)
+    i += 1
+
+for j in range(10): #-->range가 0~9까지 리스트 이때 집합이나,리스트,딕셔너리 올 수 있음
+    print(j)
+
